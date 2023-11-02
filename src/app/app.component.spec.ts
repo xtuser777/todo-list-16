1838d9ef6420700b648bfbb207af6f45cc6eec4e
Lucas Oliveira <lucaoxt@gmail.com>
2023-11-02 10:52:23 -0300

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { first } from 'rxjs';
+import { TodoSignalsService } from './services/todo-signals.service';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Todo } from './models/todo.model';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let todoSignalsService: TodoSignalsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, BrowserAnimationsModule, NoopAnimationsModule],
+      providers: [TodoSignalsService],
     });
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    todoSignalsService = TestBed.inject(TodoSignalsService);
     fixture.detectChanges();
   });
 
@@ -51,6 +59,38 @@ describe('AppComponent', () => {
         expect(event).toEqual('Todo list');
         component.handleEmitEvent();
       }
-    })
-  })
+    });
+  });
+
+  it('should create new Todo correctly and call service method', () => {
+    jest.spyOn(todoSignalsService, 'updateTodos');
+    const todo: Todo = {
+      id: 1,
+      title: 'Test',
+      description: 'Test of creation',
+      done: true,
+    };
+    component.handleCreateTodo(todo);
+    fixture.detectChanges();
+    expect(todoSignalsService.updateTodos).toHaveBeenCalledWith(todo);
+    expect(component.todoSignal()).toEqual([todo]);
+  });
+
+  it('should not render paragraph in the DOM', () => {
+    const componentDebugElement: DebugElement = fixture.debugElement;
+    const element: HTMLElement = componentDebugElement.nativeElement;
+    const paragraph = element.querySelector('p');
+
+    expect(paragraph).toBeNull();
+  });
+
+  it('should render paragraph correctly', () => {
+    component.renderTestMessage = true;
+    fixture.detectChanges();
+    const componentDebugElement: DebugElement = fixture.debugElement;
+    const paragraphDebugElement = componentDebugElement.query(By.css('p'));
+    const paragraph: HTMLElement = paragraphDebugElement.nativeElement;
+
+    expect(paragraph.textContent).toEqual('Teste de renderização');
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
+import { TodoSignalsService } from './services/todo-signals.service';
+import { Todo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +17,20 @@ export class AppComponent {
   public projectName!: string;
   @Output()
   public outputEvent = new EventEmitter<string>();
+  public title = 'To-do List';
+  public todoSignal!: WritableSignal<Array<Todo>>;
+  public renderTestMessage = false;
 
-  title = 'To-do List';
+  constructor(private todoSignalsService: TodoSignalsService) {}
 
   public handleEmitEvent(): void {
     this.outputEvent.emit(this.projectName);
+  }
+
+  public handleCreateTodo(todo: Todo): void {
+    if (todo) {
+      this.todoSignalsService.updateTodos(todo);
+      this.todoSignal = this.todoSignalsService.todosState;
+    }
   }
 }
