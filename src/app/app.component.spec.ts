abb4babe171b569de9a8bdca6ccf06ea21e00ee7
Lucas Oliveira <lucaoxt@gmail.com>
2023-11-01 23:05:42 -0300

    jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { first } from 'rxjs';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppComponent],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +22,35 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo-list-16' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-list-16');
+  // it(`should have the 'todo-list-16' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('todo-list-16');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list-16 app is running!');
+  // });
+
+  it('should set @Input() property corretly', () => {
+    component.projectName = 'Todo list';
+
+    fixture.detectChanges();
+
+    expect(component.projectName).toEqual('Todo list');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list-16 app is running!');
-  });
+  it('should emit event with @Output() decorator correctly', () => {
+    component.projectName = 'Todo list';
+
+    component.outputEvent.pipe(first()).subscribe({
+      next: (event) => {
+        expect(event).toEqual('Todo list');
+        component.handleEmitEvent();
+      }
+    })
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
@@ -11,5 +11,14 @@ import { TodoCardComponent } from './components/todo-card/todo-card.component';
   styleUrls: []
 })
 export class AppComponent {
+  @Input()
+  public projectName!: string;
+  @Output()
+  public outputEvent = new EventEmitter<string>();
+
   title = 'To-do List';
+
+  public handleEmitEvent(): void {
+    this.outputEvent.emit(this.projectName);
+  }
 }
